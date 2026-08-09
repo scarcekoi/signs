b1bb0d950067037e097560c6d3d1d87fcf202c6a
Scarce Koi <kme6qv7h@purelymail.com>
2026-08-09 01:11:26 -0400

    feat: add basic mutcd and lib

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+//! Everything in this project is based off of the [MUTCD 11th Edition](https://mutcd.fhwa.dot.gov/pdfs/11th_Edition/mutcd11theditionhl.pdf).
+
+pub mod mutcd;
---
+++ src/mutcd.rs
@@ -0,0 +1,63 @@
+//! Types for representing signs in MUTCD format.
+
+/// FHWA Standard Alphabet series.
+pub enum FhwaSeries {
+    B,
+    C,
+    D,
+    E(FhwaSeriesE),
+    F,
+}
+
+/// Unmodified and modified variants of the FHWA Series E.
+pub enum FhwaSeriesE {
+    Unmodified,
+    Modified { alternate: bool },
+}
+
+/// [Available sign shapes](https://mutcd.fhwa.dot.gov/pdfs/11th_Edition/mutcd11theditionhl.pdf#%5B%7B%22num%22%3A276%2C%22gen%22%3A0%7D%2C%7B%22name%22%3A%22FitR%22%7D%2C-154%2C194%2C766%2C792%5D) defined by the [MUTCD 11th Edition](https://mutcd.fhwa.dot.gov/pdfs/11th_Edition/mutcd11theditionhl.pdf).
+pub enum MutcdSignShape {
+    Octagon,
+    /// An equilateral triangle that points downward.
+    Triangle,
+    Circle,
+    /// An isosceles triangle with a longer axis horizontally, pointed right.
+    Pennant,
+    /// A pentagon pointed upwards.
+    Pentagon,
+    /// Two rectangles in a perpendicular “X” configuration.
+    Crossbuck,
+    Diamond,
+    Rectangle,
+    Trapezoid,
+}
+
+/// The direction for the [`Arrow`] struct.
+pub enum ArrowDirection {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+/// The arrow on the sign.
+pub struct Arrow {
+    pub direction: ArrowDirection,
+}
+
+/// The features of the sign.
+pub struct MutcdSignFeatures {
+    pub arrow: Option<Arrow>,
+}
+
+/// The sign with MUTCD features.
+pub struct MutcdSign {
+    /// Text to display on the sign.
+    pub text: String,
+    /// Which MUTCD series to use for the text on the sign.
+    pub font: FhwaSeries,
+    /// The shape of the sign.
+    pub shape: MutcdSignShape,
+    /// The features of the sign.
+    pub features: MutcdSignFeatures,
+}
