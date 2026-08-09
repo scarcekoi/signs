6e7e663d85f689cb46906e47abf83e1d18f5a19e
Scarce Koi <kme6qv7h@purelymail.com>
2026-08-09 02:19:19 -0400

    docs(lib): add sources

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 //! Everything in this project is based off of the [MUTCD 11th Edition](https://mutcd.fhwa.dot.gov/pdfs/11th_Edition/mutcd11theditionhl.pdf).
 
+//! # Sources
+//!
+//! Federal Highway Administration. (2023). U.S. Department of Transportation, Federal Highway Administration. https://mutcd.fhwa.dot.gov/pdfs/11th_Edition/mutcd11theditionhl.pdf
+
 pub mod models;
 pub mod mutcd;
